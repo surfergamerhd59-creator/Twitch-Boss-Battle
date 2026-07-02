9cdb3c1e1efa60ceab1bf491d94482674aeb0eb2
surfergamerhd59-creator <surfergamerhd59@gmail.com>
2026-07-02 20:03:16 -0300

    Update drizzle.config.ts

--- lib/db/drizzle.config.ts
+++ lib/db/drizzle.config.ts
@@ -1,1 +1,11 @@
-import { defineConfig } from "drizzle-kit";import path from "path";export default defineConfig({schema: path.join(__dirname, "./src/schema/index.ts"),dialect: "postgresql",dbCredentials: {url: "postgresql://admin:VvTOCWxSDdU9Gj3Ye5rwyzB7eeiTHtt0@render.com",ssl: { rejectUnauthorized: false }}});
+import { defineConfig } from "drizzle-kit";
+import path from "path";
+
+export default defineConfig({
+  schema: path.join(__dirname, "./src/schema/index.ts"),
+  dialect: "postgresql",
+  dbCredentials: {
+    url: "postgresql://admin:VvTOCWxSDdU9Gj3Ye5rwyzB7eeiTHtt0@render.com",
+    ssl: { rejectUnauthorized: false }
+  }
+});
